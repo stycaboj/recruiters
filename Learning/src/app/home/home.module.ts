f9c7ffbec31e77aa74295e0c2ce7bbed35640795
Artem <artmglbv@gmail.com>
2024-01-28 03:11:31 +0300

    fix: delete top-recruiters and top-vacancies

--- Learning/src/app/home/home.module.ts
+++ Learning/src/app/home/home.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from "@angular/core";
 import { MatCardModule } from '@angular/material/card';
 
 import { HomeRoutingModule } from "./home-routing.module";
-import { TopRecruitersComponent } from "./top-recruiters/top-recruiters.component";
-import { TopVacanciesComponent } from "./top-vacancies/top-vacancies.component";
 import { HomeComponent } from "./home.component";
 import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
     declarations: [
-        TopRecruitersComponent,
-        TopVacanciesComponent,
         HomeComponent,
         CardListComponent,
     ],
